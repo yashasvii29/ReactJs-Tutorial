b17cf26cf4e221ee21b294431a2c24e3336671db
yashasvi <yashasviagrawal29@gmail.com>
2023-12-27 01:59:03 +0530

    add first-app

---
+++ first-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+        <div>
+          <p>Hi, THis is Yashasvi</p>
+        </div>
+    </div>
+  )
+}
+
+export default App
